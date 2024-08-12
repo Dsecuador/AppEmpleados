6553b5cdac3986f9ab47e3ff9c52e6bdbafd5527
Renee <dsecuador.media@gmail.com>
2024-08-12 14:32:45 -0500

    Aplicación EMPLEADOS, Integracion de Software

---
+++ Controllers/AccesoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Mvc;
+using System.Text;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+{
+
+}
+
+namespace AplicacionEmpleados.Controllers
+{
+    public class AccesoController
+    {
+        // Entidad para conectarse entre la base de datos y los objetos de la misma
+        private string cadena = "data source";
+
+        // data source; nombre del servidor localhost
+        // Catalogo: Nombre de la base de datos: Empleados
+        // Data source: el servidor o equipo donde está
+        // USer ID es el usuario
+        // Password
+        // windows autentication= true
+
+
+    }
+}
